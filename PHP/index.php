6a8b35e9f685a228219f57e5ce34c099dcfdad3c
Dainty Wilder <mamiamman@gmail.com>
2026-01-12 17:30:50 +0530

    Create index.php

---
+++ PHP/index.php
@@ -0,0 +1,35 @@
+<html>
+    <body>
+        <?php
+        $a = $_POST['a'];
+        $b = $_POST['b'];
+        $c = $_POST['c'];
+        $d = $_POST['d'];
+        $e = $_POST['e'];
+        $f = $_POST['f'];
+        $g = $_POST['g'];
+        $h = $_POST['h'];
+        $i = $_POST['i'];
+        $j = $_POST['j'];
+        $k = $_POST['k'];
+        $l = $_POST['l'];
+        $m = $_POST['m'];
+        echo "Value is   " .$a."<br>"; 
+        echo "Value is   " .$b."<br>"; 
+        echo "Value is   " .$c."<br>"; 
+        echo "Value is   " .$d."<br>"; 
+        echo "Value is   " .$e."<br>"; 
+        echo "Value is   " .$f."<br>"; 
+        echo "Value is   " .$g."<br>"; 
+        echo "Value is   " .$h."<br>"; 
+        echo "Value is   " .$i."<br>";
+        foreach($j as $x) {
+        echo "Value is   " .$x."<br>"; 
+        }
+        echo "Value is   " .$k."<br>"; 
+        echo "Value is   " .$l."<br>"; 
+        echo "Value is   " .$m."<br>"; 
+              ?>    
+        
+    </body>
+</html>
